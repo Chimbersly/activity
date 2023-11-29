f1214f80343b1699d3cb3fcc8ec9182ae09e05ce
Chimbersly <152365620+Chimbersly@users.noreply.github.com>
2023-11-29 14:30:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write('HELLO WORLD')
+st.set_page_config(page_title="My Webpage", page_icon=":tada", layout="wide")
+
+with st.container():
+  st.subhader("Hi, I am Chimbersly Perez :wave:")
+  st.write("Still improving this site")
